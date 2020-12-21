d04bedcb2612131280760eeb7d044033bef0d841
RedHelium <mrfacegames@gmail.com>
2020-12-21 18:47:38 +1000

    Added main, auction and samples pages. Added Naviation grid and Menu control

--- RoadHouse Studio/RoadHouse Studio/MainWindow.xaml.cs
+++ RoadHouse Studio/RoadHouse Studio/MainWindow.xaml.cs
@@ -1,28 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RoadHouse_Studio.Pages;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RoadHouse_Studio
-{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+{   
+
     public partial class MainWindow : Window
     {
+        private readonly Main mainPage = new Main();
+        private readonly Auction auctionPage = new Auction();
+        private readonly Samples samplesPage = new Samples();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            NavigationFrame.Navigate(mainPage);
+
+            InitNavigationEvents();
+            InitMenuEvents();
+           
         }
+
+        private void InitNavigationEvents()
+        {
+            InitNavigationButton(Nav_Main, mainPage);
+            InitNavigationButton(Nav_Auction, auctionPage);
+            InitNavigationButton(Nav_Samples, samplesPage);           
+        }
+
+        private void InitMenuEvents()
+        {    
+            InitNavigationMenuButton(Menu_Main, mainPage);
+            InitNavigationMenuButton(Menu_Auction, auctionPage);
+            InitNavigationMenuButton(Menu_Samples, samplesPage);
+        }
+
+        private void InitNavigationButton(Button button, Page page)
+        => button.Click += (sender, EventArgs) => { Navigate(sender, EventArgs, page); };
+
+        private void InitNavigationMenuButton(MenuItem item, Page page)
+        => item.Click += (sender, EventArgs) => { Navigate(sender, EventArgs, page); };
+
+        public void Navigate(object sender, RoutedEventArgs args, Page page) => NavigationFrame.Navigate(page);
+        
     }
 }
---
+++ RoadHouse Studio/RoadHouse Studio/Pages/Main.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows.Controls;
+
+namespace RoadHouse_Studio.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
